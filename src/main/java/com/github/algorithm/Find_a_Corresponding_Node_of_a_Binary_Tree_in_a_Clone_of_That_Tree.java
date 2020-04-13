641ab84a4689efa6d31a576999ae6276eeb61c78
武文韬 <wuwentao@yidian-inc.com>
2020-04-13 23:21:59 +0800

    add global bean

---
+++ src/main/java/com/github/algorithm/Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree.java
@@ -0,0 +1,17 @@
+package com.github.algorithm;
+
+
+import com.github.bean.TreeNode;
+
+/**
+ * @author : wuwentao
+ * @date : 2020/4/13
+ */
+public class Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree {
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+
+
+        return null;
+    }
+}
---
+++ src/main/java/com/github/bean/TreeNode.java
@@ -0,0 +1,12 @@
+package com.github.bean;
+
+/**
+ * @author : wuwentao
+ * @date : 2020/4/13
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
