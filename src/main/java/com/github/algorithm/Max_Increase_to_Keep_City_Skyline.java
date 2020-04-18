5ff577865612a1df2a910b6e682ee78adcf124b7
武文韬 <wuwentao@yidian-inc.com>
2020-04-18 22:40:42 +0800

    update answer

--- src/main/java/com/github/algorithm/Max_Increase_to_Keep_City_Skyline.java
+++ src/main/java/com/github/algorithm/Max_Increase_to_Keep_City_Skyline.java
@@ -8,6 +8,7 @@ public class Max_Increase_to_Keep_City_Skyline {
 
     /**
      * 找到正面看和侧面看的高度数组，然后取各点的最小值
+     * 优化PS:一次遍历得到rows和cols的值
      * @param grid
      * @return
      */
@@ -19,19 +20,24 @@ public class Max_Increase_to_Keep_City_Skyline {
 
         for (int i = 0;i < grid.length;i ++) {
             for (int j = 0;j < grid[i].length;j ++) {
-                if (grid[i][j] > rows[i]) {
-                    rows[i] = grid[i][j];
-                }
+//                if (grid[i][j] > rows[i]) {
+//                    rows[i] = grid[i][j];
+//                }
+                rows[i] = Math.max(grid[i][j], rows[i]);
+//                if (grid[i][j] > cols[j]) {
+//                    cols[j] = grid[i][j];
+//                }
+                cols[j] = Math.max(grid[i][j], cols[j]);
             }
         }
 
-        for (int j = 0;j < grid[0].length;j ++) {
-            for (int i = 0;i < grid.length;i ++) {
-                if (grid[i][j] > cols[j]) {
-                    cols[j] = grid[i][j];
-                }
-            }
-        }
+//        for (int j = 0;j < grid[0].length;j ++) {
+//            for (int i = 0;i < grid.length;i ++) {
+//                if (grid[i][j] > cols[j]) {
+//                    cols[j] = grid[i][j];
+//                }
+//            }
+//        }
 
         int sum = 0;
         for (int i = 0;i < rows.length; i ++) {
