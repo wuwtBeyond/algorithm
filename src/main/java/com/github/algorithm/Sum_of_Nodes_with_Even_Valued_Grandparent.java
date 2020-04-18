ce8aff0dcd52174bd84e32986ba0f5bbf005ffa2
武文韬 <wuwentao@yidian-inc.com>
2020-04-18 16:19:49 +0800

    add answer

---
+++ src/main/java/com/github/algorithm/Sum_of_Nodes_with_Even_Valued_Grandparent.java
@@ -0,0 +1,46 @@
+package com.github.algorithm;
+
+import com.github.bean.TreeNode;
+
+/**
+ * @author : wuwentao
+ * @date : 2020/4/17
+ */
+public class Sum_of_Nodes_with_Even_Valued_Grandparent {
+
+    private int sum = 0;
+
+    public int sumEvenGrandparent(TreeNode root) {
+        dfs(root, null, null);
+
+        return sum;
+    }
+
+    /**
+     * 将当前节点、父节点、祖父节点的关系递归传递下去
+     * @param current
+     * @param parent
+     * @param grandParent
+     */
+    private void dfs(TreeNode current, TreeNode parent, TreeNode grandParent) {
+        if (current == null) return ;
+
+        if (grandParent != null && grandParent.val % 2 == 0) {
+            sum += current.val;
+        }
+
+        dfs(current.left, current, parent);
+        dfs(current.right, current, parent);
+    }
+
+
+//    private void dfs(TreeNode root, int depth) {
+//        if (root == null) return;
+//
+//        if (depth == grandParentDepth+2) {
+//            sum += root.val;
+//        }
+//
+//
+//    }
+}
--- src/main/java/com/github/test.java
+++ src/main/java/com/github/test.java
@@ -3,6 +3,7 @@ package com.github;
 import com.github.algorithm.Deepest_Leaves_Sum;
 import com.github.algorithm.Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree;
 import com.github.algorithm.Group_the_People_Given_the_Group_Size_They_Belong_To;
+import com.github.algorithm.Sum_of_Nodes_with_Even_Valued_Grandparent;
 import com.github.bean.TreeNode;
 
 /**
@@ -28,7 +29,7 @@ public class test {
     public static void main(String[] args) {
 //        Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree main = new Find_a_Corresponding_Node_of_a_Binary_Tree_in_a_Clone_of_That_Tree();
 //
-        TreeNode t1 = new TreeNode(7);
+        TreeNode t1 = new TreeNode(6);
         TreeNode t2 = new TreeNode(4);
         TreeNode t3 = new TreeNode(3);
         t1.left = t2;
@@ -39,14 +40,16 @@ public class test {
 
         t3.left = t4;
         t3.right = t5;
-//          7
+//          6
 //       4    3
 //          6   19
 //        System.out.println(main.getTargetCopy(t1, t1, t5).val);
 
 //        Group_the_People_Given_the_Group_Size_They_Belong_To main = new Group_the_People_Given_the_Group_Size_They_Belong_To();
 //        main.groupThePeople(new int[]{2,2,1,1,1,1,1,1});
-        Deepest_Leaves_Sum main = new Deepest_Leaves_Sum();
-        main.deepestLeavesSum(t1);
+//        Deepest_Leaves_Sum main = new Deepest_Leaves_Sum();
+//        main.deepestLeavesSum(t1);
+        Sum_of_Nodes_with_Even_Valued_Grandparent main = new Sum_of_Nodes_with_Even_Valued_Grandparent();
+        main.sumEvenGrandparent(t1);
     }
 }
