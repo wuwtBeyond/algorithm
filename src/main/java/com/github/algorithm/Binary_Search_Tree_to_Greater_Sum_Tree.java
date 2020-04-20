dfef505d3fbf8d5fc5368e754781b293b4e513a6
武文韬 <wuwentao@yidian-inc.com>
2020-04-20 23:39:49 +0800

    add answer

---
+++ src/main/java/com/github/algorithm/Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -0,0 +1,16 @@
+package com.github.algorithm;
+
+import com.github.bean.TreeNode;
+
+/**
+ * @author : wuwentao
+ * @date : 2020/4/20
+ */
+public class Binary_Search_Tree_to_Greater_Sum_Tree {
+
+    public TreeNode bstToGst(TreeNode root) {
+
+
+        return null;
+    }
+}
