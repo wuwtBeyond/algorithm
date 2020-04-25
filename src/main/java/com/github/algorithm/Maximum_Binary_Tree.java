2d3dfaa9e0adfd3d60f3ed566ab4011eb2e97b4f
武文韬 <wuwentao@yidian-inc.com>
2020-04-25 22:06:49 +0800

    update answer

--- src/main/java/com/github/algorithm/Maximum_Binary_Tree.java
+++ src/main/java/com/github/algorithm/Maximum_Binary_Tree.java
@@ -13,12 +13,23 @@ public class Maximum_Binary_Tree {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         if (nums == null || nums.length <= 0) return null;
 
-        TreeNode root = dfs(nums, 0, nums.length);
+        TreeNode root = recursive(nums, 0, nums.length);
 
         return root;
     }
 
-    private TreeNode dfs(int[] nums, int left, int right) {
+    // 3,2,1,6,0,5
+
+    /**
+     * 递归建立各级节点的关系，最后返回初始节点
+     * 注意边界，在left和right范围内找到最大点
+     * 此处设计为左闭右开，所以在右侧遍历时需要+1,否则会重复找到最大值还是自己
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private TreeNode recursive(int[] nums, int left, int right) {
         if (left >= right ) return null;
 
         int max_index = findMaxNumIndex(nums, left, right);
@@ -27,8 +38,8 @@ public class Maximum_Binary_Tree {
 //            p = node;
 //        }
 
-        node.left = dfs(nums, 0, max_index);
-        node.right = dfs(nums, max_index, nums.length);
+        node.left = recursive(nums, left, max_index);
+        node.right = recursive(nums, max_index+1, right);
 
         return node;
     }
@@ -42,12 +53,16 @@ public class Maximum_Binary_Tree {
      * @return
      */
     private int findMaxNumIndex(int[] nums, int left, int right) {
-        int max = nums[left];
+        int max = nums[left], max_index = left;
 
         for (int i = left;i < right;i ++) {
-            max = Math.max(max, nums[i]);
+//            max = Math.max(max, nums[i]);
+            if (nums[i] > max) {
+                max  = nums[i];
+                max_index = i;
+            }
         }
 
-        return max;
+        return max_index;
     }
 }
