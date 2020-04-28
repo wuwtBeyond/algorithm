912edb1098a4809c3658717585a96e98dd8c7abe
武文韬 <wuwentao@yidian-inc.com>
2020-04-28 22:19:09 +0800

    update answer

--- src/main/java/com/github/algorithm/Construct_Binary_Search_Tree_from_Preorder_Traversal.java
+++ src/main/java/com/github/algorithm/Construct_Binary_Search_Tree_from_Preorder_Traversal.java
@@ -11,15 +11,32 @@ public class Construct_Binary_Search_Tree_from_Preorder_Traversal {
     public TreeNode bstFromPreorder(int[] preorder) {
         if (preorder == null || preorder.length <= 0) return null;
 
-        TreeNode root = recurive(preorder, 0, preorder.length);
+        TreeNode root = recursive(preorder,0,  preorder.length);
 
         return root;
     }
 
-    private TreeNode recurive(int[] preorder, int left, int right) {
+    /**
+     * 设计递归递推式，定义好边界即可 与之前题目类似
+     * @param preorder
+     * @param left
+     * @param right
+     * @return
+     */
+    private TreeNode recursive(int[] preorder, int left, int right) {
+        if (left >= right) return null;
 
+        TreeNode node = new TreeNode(preorder[left]);
+        int i = left + 1;
+        for (;i < right;i ++) {
+            if (preorder[i] > preorder[left]) {
+                break;
+            }
+        }
 
+        node.left = recursive(preorder, left+1, i);
+        node.right = recursive(preorder, i, right);
 
-        return null;
+        return node;
     }
 }
