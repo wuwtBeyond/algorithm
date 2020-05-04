409f523aed42d979cef6ca5aadb5f80ce5336249
武文韬 <wuwentao@yidian-inc.com>
2020-05-04 12:53:24 +0800

    update asnwer

--- src/main/java/com/github/algorithm/Reduce_Array_Size_to_The_Half.java
+++ src/main/java/com/github/algorithm/Reduce_Array_Size_to_The_Half.java
@@ -1,7 +1,6 @@
 package com.github.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author : wuwentao
@@ -10,7 +9,25 @@ import java.util.List;
 public class Reduce_Array_Size_to_The_Half {
 
     public int minSetSize(int[] arr) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
         List<Integer> list = new ArrayList<>();
+        for (int value : map.values()) {
+            list.add(value);
+        }
+        Collections.sort(list);
+
+        int count = 0, res = 0;
+        for (int i = list.size()-1;i >= 0;i --) {
+                count += list.get(i);
+                res ++;
+                if (count >= arr.length/2) {
+                    return res;
+                }
+        }
 
         return 0;
     }
