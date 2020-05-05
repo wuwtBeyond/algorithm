21cdca3628eb3968408ffcb69c207d64c56ccdcd
武文韬 <wuwentao@yidian-inc.com>
2020-05-06 00:04:35 +0800

    update answer

---
+++ src/main/java/com/github/algorithm/Maximum_sum_circular_subarray.java
@@ -0,0 +1,20 @@
+package com.github.algorithm;
+
+/**
+ * @author : wuwentao
+ * @date : 2020/5/6
+ */
+public class Maximum_sum_circular_subarray {
+
+    /**
+     * 通过分析，最优解是比较两者的最大值
+     * 1，最大连续子数组的和
+     * 2，总和减去最小连续子数组的和
+     * @param A
+     * @return
+     */
+    public int maxSubarraySumCircular(int[] A) {
+
+        return 0;
+    }
+}
