a5a5d1fd7797870f5fd17750409cd0935e6671f1
wuwtBeyond <1204LUCKYstar>
2020-11-29 22:39:48 +0800

    更新

---
+++ src/main/java/com/github/algorithm/greedy/RemoveKdigits.java
@@ -0,0 +1,42 @@
+package com.github.algorithm.greedy;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class RemoveKdigits {
+
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<>();
+        int length = num.length();
+        for (int i = 0; i < length; ++i) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(digit);
+        }
+
+        for (int i = 0; i < k; ++i) {
+            deque.pollLast();
+        }
+
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        while (!deque.isEmpty()) {
+            char digit = deque.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ret.append(digit);
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
+    }
+
+    public static void main(String[] args) {
+        RemoveKdigits removeKdigits = new RemoveKdigits();
+        removeKdigits.removeKdigits("10", 1);
+    }
+}
+
